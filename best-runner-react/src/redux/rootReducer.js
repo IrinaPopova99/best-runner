ebd9b3fc144a5c1ba51ad694b7465c490aad77ae
IrinaPopova99 <janneteldenorr@gmail.com>
2021-03-30 22:59:03 +0700

    Remove frontend folder

--- best-runner-react/src/redux/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { workoutReducer } from './workouts/reducer';
-
-export const rootReducer = combineReducers({
-    workoutReducer,
-});
--- best-runner-react/src/utilities/validations/validations.js
+++
@@ -1,9 +0,0 @@
-export const validations = (nameItem, valueItem, required = false, maxLength = 10000, valueIsNumber = false, pattern = ".") => {
-    return ({
-        // validate: { value: valueItem => (valueItem === getValues().email) || 'Email confirmation error!' },
-        required: { value: required, message: "Это поле обязательно" },
-        maxLength: { value: maxLength, message: `Максимальная длина ${maxLength}` },
-        // valueIsNumber: { value: valueIsNumber, message: `Используйте только числа` },
-        pattern: pattern
-    });
-}
